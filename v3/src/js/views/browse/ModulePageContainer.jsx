b1cbfbce747dea13a8d7a015a58df4b8c85e1394
Swiftkage <lim.yu.xi@hotmail.com>
2017-06-26 12:09:04 +0800

    Module page
    View and edit module information

--- v3/src/js/views/browse/ModulePageContainer.jsx
+++ v3/src/js/views/browse/ModulePageContainer.jsx
@@ -1,18 +1,18 @@
 // @flow
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import DocumentTitle from 'react-document-title';
 import config from 'config';
 
-import { loadModule } from 'actions/moduleBank';
-import { addModule, removeModule } from 'actions/timetables';
-import type { Module } from 'types/modules';
-import type { FetchRequest } from 'types/reducers';
-import { formatExamDate } from 'utils/modules';
-import type { TimetableConfig } from 'types/timetables';
+import {loadModule} from 'actions/moduleBank';
+import {addModule, removeModule} from 'actions/timetables';
+import type {Module} from 'types/modules';
+import type {FetchRequest} from 'types/reducers';
+import {formatExamDate} from 'utils/modules';
+import type {TimetableConfig} from 'types/timetables';
 import AddModuleButton from './AddModuleButton';
 import RemoveModuleButton from './RemoveModuleButton';
-
+import InlineEdit from 'react-edit-inline';
 
 
 var firebase = require("firebase");
@@ -41,8 +41,23 @@ type Props = {
 
 export class ModulePageContainer extends Component {
 
+  constructor() {
+    super();
+    this.dataChanged = this.dataChanged.bind(this);
+    this.state = {
+      usefulLinks: '',
+      feedbacks: '',
+      projects: '',
+      funFacts: '',
+    }
+  }
+
   componentDidMount() {
     this.loadModuleInformation(this.props);
+    this.readUsefulLinks();
+    this.readFeedbacks();
+    this.readProjects();
+    this.readFunFacts();
   }
 
   componentWillReceiveProps(nextProps: Props) {
@@ -68,7 +83,7 @@ export class ModulePageContainer extends Component {
       this.props.module.History
         .filter(h => h.ExamDate != null)
         .sort((a, b) => a.Semester - b.Semester)
-        .map(h => ({ semester: h.Semester, date: h.ExamDate || '' })))
+        .map(h => ({semester: h.Semester, date: h.ExamDate || ''})))
       : [];
   }
 
@@ -80,16 +95,68 @@ export class ModulePageContainer extends Component {
     return timetables[semester] && !!timetables[semester][module.ModuleCode];
   }
 
-  readUsefulLinks(module: Module){
-    var ref = firebase.database().ref(module.ModuleCode+"/Useful Links/");
-    console.log('asdsasadasdasdsa');
-    ref.on('value', function(snapshot) {
-      var result = snapshot.val().toString();
-      console.log(result);
-      return ref.once("value");
-    }, function (error) {
-      console.log('Error: ' + error.code);
-      return 'There is currently nothing';
+  readUsefulLinks() {
+    var ref = firebase.database().ref(this.props.module.ModuleCode + '/usefulLinks/');
+
+    ref.on('value', function (snapshot) {
+      var obj = snapshot.val();
+
+      this.setState({
+        usefulLinks: snapshot.val()
+      });
+
+    }.bind(this));
+  }
+
+
+  readFeedbacks() {
+    var ref = firebase.database().ref(this.props.module.ModuleCode + '/feedbacks/');
+
+    ref.on('value', function (snapshot) {
+      var obj = snapshot.val();
+
+      this.setState({
+        feedbacks: snapshot.val()
+      });
+
+    }.bind(this));
+  }
+
+  readProjects() {
+    var ref = firebase.database().ref(this.props.module.ModuleCode + '/projects/');
+
+    ref.on('value', function (snapshot) {
+      var obj = snapshot.val();
+
+      this.setState({
+        projects: snapshot.val()
+      });
+
+    }.bind(this));
+  }
+
+  readFunFacts() {
+    var ref = firebase.database().ref(this.props.module.ModuleCode + '/funFacts/');
+
+    ref.on('value', function (snapshot) {
+      var obj = snapshot.val();
+
+      this.setState({
+        funFacts: snapshot.val()
+      });
+
+    }.bind(this));
+  }
+
+  dataChanged(data) {
+    // data = { description: "New validated text comes here" }
+    // Update your model from here
+    console.log(data);
+    this.setState({...data});
+    var ref = firebase.database().ref(this.props.module.ModuleCode + '/');
+
+    ref.update({
+      ...data
     });
   }
 
@@ -113,19 +180,17 @@ export class ModulePageContainer extends Component {
       .map(sem => `Semester ${sem}`)
       .join(', ');
 
-    const usefulLinks = this.readUsefulLinks(module);
-
     const addOrRemoveToTimetableLinks = this.semestersOffered().map(
       semester => (
         this.moduleHasBeenAdded(module, semester) ?
           <RemoveModuleButton key={semester} semester={semester} onClick={() =>
             this.props.removeModule(semester, module.ModuleCode)
-          } />
+          }/>
           :
           <AddModuleButton key={semester} semester={semester} onClick={() =>
               this.props.addModule(semester, module.ModuleCode)
-            } />
-        ),
+            }/>
+      ),
     );
 
     return (
@@ -168,7 +233,7 @@ export class ModulePageContainer extends Component {
                 <dt className="col-sm-3">Semesters Offered</dt>
                 <dd className="col-sm-9">{semsOffered}</dd>
 
-                <dt className="col-sm-3">Offical Links</dt>
+                <dt className="col-sm-3">Official Links</dt>
                 <dd className="col-sm-9">
                   <ul className="nm-footer-links">
                     {ivleLink ? <li><a href={ivleLink}>IVLE</a></li> : null}
@@ -177,14 +242,20 @@ export class ModulePageContainer extends Component {
                 </dd>
 
 
-
               </dl>
 
               <hr />
               <dt className="col-sm-3">Useful Links</dt>
-              <dd className="col-sm-9">{usefulLinks}</dd>
+              <dd className="col-sm-9">{ this.state.usefulLinks != '' ? <InlineEdit text={this.state.usefulLinks} activeClassName="editing" change={this.dataChanged} paramName="usefulLinks" /> : <font color="red">There is nothing to show here :( Try adding something!</font> }</dd>
 
+              <dt className="col-sm-3">Past lecturers/tutors feedback</dt>
+              <dd className="col-sm-9">{ this.state.feedbacks != '' ? <InlineEdit text={this.state.feedbacks} activeClassName="editing" change={this.dataChanged} paramName="feedbacks" /> : <font color="red">There is nothing to show here :( Try adding something!</font> }</dd>
 
+              <dt className="col-sm-3">Outstanding projects</dt>
+              <dd className="col-sm-9">{ this.state.projects != '' ? <InlineEdit text={this.state.projects} activeClassName="editing" change={this.dataChanged} paramName="projects" /> : <font color="red">There is nothing to show here :( Try adding something!</font> }</dd>
+
+              <dt className="col-sm-3">Fun facts</dt>
+              <dd className="col-sm-9">{ this.state.funFacts != '' ? <InlineEdit text={this.state.funFacts} activeClassName="editing" change={this.dataChanged} paramName="funFacts" /> : <font color="red">There is nothing to show here :( Try adding something!</font> }</dd>
 
 
             </div> : null
